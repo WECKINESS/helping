588b3c09e5306be07695151cb5b19e21ff867add
WECKINESS <88478838+WECKINESS@users.noreply.github.com>
2022-10-31 00:04:25 +0800

    Update help.py

--- help.py
+++ help.py
@@ -1,19 +1,16 @@
-import tkinter as tk
-import time
-import csv
+        import tkinter as tk
 import sqlite3
-from turtle import update
 
 class Helping:
     def __init__(self):
-        self.out = ''
+        self.out = '^-^'
 
         self.root = tk.Tk()
         self.root.title('互帮互助')
 
         self.conn = sqlite3.connect('helping.db')
         self.cur = self.conn.cursor()
-        self.cur.execute('create table if not exists helping (name text not null, quantity integer);')
+        self.cur.execute('create table if not exists helping (name text not null, quantity text);')
         
         self.lb_item = tk.Label(self.root,text='请输入物品名',fg='black',bg='white')
         self.lb_item.grid(row=0,column=0,columnspan=2)  
@@ -41,70 +38,78 @@ class Helping:
 
         self.root.mainloop()
 
+    #添加物品
     def on(self):
         it = self.entry_item.get()
         num = self.entry_number.get()
         if it == '':
-            self.out = '请输入物品名'
+            self.lb_putout.config(text= '请输入物品名') 
         else:
             if num == '':
-                self.out = '请输入物品数量'
+                self.lb_putout.config(text='请输入物品数量')
             else:
                 if num.isdigit():
                     self.cur.execute('select * from helping where name like "%s"'  %it)
-                    if self.cur.fetchone() is None:
+                    if (fetch :=self.cur.fetchone()) is None:
                         self.cur.execute('insert into helping values("%s","%s")' %(it,num))
                         self.conn.commit()
+                        self.lb_putout.config(text = '输入成功')
                     else:
                         #print(type(self.cur.fetchone()))
-                        it_ = self.cur.fetchone()
-                        num = str(int(it_[1]) + int(num))
-                        self.cur.execute('update helping set quantity="%s" where name="%s"' %(it,num))
+                        num = str(int(fetch[1]) + int(num))
+                        self.cur.execute('update helping set quantity="%s" where name="%s"' %(num,it))
                         self.conn.commit()
+                        self.lb_putout.config(text = '输入成功')
                         
                 else:
-                    self.out = '物品数量应为正整数'
-                           
+                    self.lb_putout.config(text= '物品数量应为正整数')
+
+    #删除物品                           
     def off(self):
         it = self.entry_item.get()
         num = self.entry_number.get()
         if it == '':
-            self.out = '请输入物品名'
+            self.lb_putout.config(text='请输入物品名')
         else:
             if num == '':
-                self.out = '请输入物品数量'
+                self.lb_putout.config(text = '请输入物品数量')
             else:
                 if num.isdigit():
                     self.cur.execute('select * from helping where name like "%s"'  %it)
-                    if self.cur.fetchone() is None:
-                        self.out = '物品不存在'  
+                    if (fetch:=self.cur.fetchone()) is None:
+                        self.lb_putout.config(text = '物品不存在')
                     else:
-                        it_ = self.cur.fetchone() 
-                        if str(num) > str(it_[1]):
-                            self.out = '物品数目不足'
-                        elif str(num) == str(it_[1]):
-                            self.cur.execute('delete from helping where name = "%s" limit 1' %it)
+                        if str(num) > str(fetch[1]):
+                            self.lb_putout.config(text = '物品数目不足')
+                        elif str(num) == str(fetch[1]):
+                            self.cur.execute('delete from helping where name = "%s" ' %it)
                             self.conn.commit()
+                            self.lb_putout.config(text = '删除成功')
                         else:
-                            num = str(int(it_[1]) - int(num))
-                            self.cur.execute('update helping set quantity="%s" where name="%s"' %(it,num))
+                            num = str(int(fetch[1]) - int(num))
+                            self.cur.execute('update helping set quantity="%s" where name="%s"' %(num,it))
                             self.conn.commit() 
+                            self.lb_putout.config(text = '删除成功')
                 else:
-                    self.out = '物品数量应为正整数'
+                    self.lb_putout.config(text = '物品数量应为正整数')
                  
-
+    #查找物品
     def find(self):
         it = self.entry_item.get()
         if it == '':
-            self.out = '请输入物品名'
+            self.lb_putout.config(text = '请输入物品名')
         else:
-            self.cur.execute('select * from helping where name like "%%%s%%"' % it)
-            for record in self.cur.fetchall():self.out = self.out + record + '\n' 
-            
+            self.out=''
+            self.cur.execute('select * from helping where name like "%%%s%%"' %it)
+            for record in self.cur.fetchall():self.out = self.out + str(record) + '\n' 
+            self.lb_putout.config(text=self.out)            
 
+    #显示物品清单
     def lst(self):
+        self.out = ''
         self.cur.execute('select * from helping')
-        for record in self.cur.fetchall():self.out = self.out + record + '\n' 
+        for record in self.cur.fetchall():self.out = self.out + str(record) + '\n' 
+        self.lb_putout.config(text=self.out)
 
 Helping = Helping()
 
